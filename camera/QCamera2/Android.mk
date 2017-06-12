48b9ed9301cd214e8f3d47869280e2baa0334f64
Jiyong Park <jiyong@google.com>
2017-06-12 17:16:36 +0900

    land: Replace libcamera_client to android.hardware.camera.common@1.0-helper
    libcamera_client is not a VNDK library and thus not available to
    vendors. These vendor libs were using the library due to CameraMetadata
    and CameraParamters classes. The classes are now available via
    .common@1.0-helper library which is available to vendors. Thus switching
    to the lib.
    
    Bug: 62523241
    Test: 2017 pixel devices builds and boots. Camera functionalities work.
    Change-Id: I16d0fcf94b54c480b8e817c63341fb9f45118867

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -35,7 +35,6 @@ LOCAL_SRC_FILES += \
         HAL/QCameraPostProc.cpp \
         HAL/QCamera2HWICallbacks.cpp \
         HAL/QCameraParameters.cpp \
-        HAL/CameraParameters.cpp \
         HAL/QCameraParametersIntf.cpp \
         HAL/QCameraThermalAdapter.cpp
 
